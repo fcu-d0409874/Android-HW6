87e0e815efc8412c2ada0cd4d8871139e11b184c
fcu-d0409874 <jim23693728@gmail.com>
2017-05-14 20:32:52 +0800

    forgot to upload

---
+++ app/src/main/java/com/example/jim23/homework6/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.jim23.homework6;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    SQLiteDatabase db;
+    ArrayList<String> titlelist;
+    ListView lv;
+    Button btn_add;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_add = (Button)findViewById(R.id.button) ;
+        btn_add.setOnClickListener(addnote);
+
+        lv = (ListView) findViewById(R.id.notelist);
+        lv.setOnItemClickListener(iclick);
+        lv.setOnItemLongClickListener(ilclick);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        db.close();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        DBOpenHelper openhelper = new DBOpenHelper(this);
+        db = openhelper.getWritableDatabase();
+
+        titlelist = NoteDB.getTitleList(db);
+        ArrayAdapter<String> adapter =new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, titlelist);
+        lv.setAdapter(adapter);
+    }
+
+    View.OnClickListener addnote = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this,
+                    NoteEditor.class);
+            intent.putExtra("NOTEPOS", -1);
+            startActivity(intent);
+        }
+    };
+
+    AdapterView.OnItemClickListener iclick = new AdapterView.OnItemClickListener() {
+
+        @Override
+        public void onItemClick(AdapterView<?> av, View v,
+                                int position, long id) {
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this,
+                    NoteEditor.class);
+            intent.putExtra("NOTEPOS", position);
+            startActivity(intent);
+        }
+    };
+
+    AdapterView.OnItemLongClickListener ilclick = new AdapterView.OnItemLongClickListener() {
+        @Override
+        public boolean onItemLongClick(AdapterView<?> av, View v,
+                                       int position, long id) {
+            String title = titlelist.get(position);
+            NoteDB.delNote(db, title);
+            titlelist = NoteDB.getTitleList(db);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                    (MainActivity.this,
+                            android.R.layout.simple_list_item_1, titlelist);
+            lv.setAdapter(adapter);
+            return false;
+        }
+
+    };
+
+}
